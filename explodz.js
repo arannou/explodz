70ccef5443548899748055fb6b11f18031f796e8
Alicia Rannou <alicia.rannou23@gmail.com>
2022-05-09 16:01:57 +0200

    Fix toogle face or not with click

--- explodz.js
+++ explodz.js
@@ -13,7 +13,7 @@ void(
         var position = " position:absolute;" + topOrBottom + ":0;" + leftOrRight + ":0;"
 		var rotate = " rotateX(" + rX + "deg) rotateY(" + rY + "deg)";
 		var transform = "transform:" + rotate +";";
-		var div = "<div style='" + common + visual + dimensions + transform + position +"'></div>";
+		var div = "<div class='dom3d' style='" + common + visual + dimensions + transform + position +"'></div>";
 		return div;
 	}
 
@@ -44,23 +44,19 @@ void(
 				}
 
 				traverse(childNode, depth + 1, elementBodyOffsetLeft, elementBodyOffsetTop);
-
+                color = getColour(depth);
 				// top
-                facesHTML += getFaceHTML("top", "left", elementBodyOffsetLeft + childNode.offsetLeft,
-						elementBodyOffsetTop + childNode.offsetTop, (depth + 1) * STEP,
-						childNode.offsetWidth, STEP, 270, 0, getColour(depth));
+                facesHTML += getFaceHTML("top", "left",
+						childNode.offsetWidth, STEP, 270, 0, color);
 				// right
-				facesHTML += getFaceHTML("top", "right", elementBodyOffsetLeft + childNode.offsetLeft + childNode.offsetWidth, 
-						elementBodyOffsetTop + childNode.offsetTop, (depth + 1) * STEP,
-						childNode.offsetHeight, STEP, 270, 90, getColour(depth));
+				facesHTML += getFaceHTML("top", "right",
+						childNode.offsetHeight, STEP, 270, 90, color);
 				// bottom
-				facesHTML += getFaceHTML("bottom", "left", elementBodyOffsetLeft + childNode.offsetLeft,
-						elementBodyOffsetTop + childNode.offsetTop + childNode.offsetHeight, (depth + 1) * STEP,
-						childNode.offsetWidth, STEP, 90, 0, getColour(depth));
+				facesHTML += getFaceHTML("bottom", "left",
+						childNode.offsetWidth, STEP, 90, 0, color);
 				// left
-				facesHTML += getFaceHTML("top", "left", elementBodyOffsetLeft + childNode.offsetLeft,
-						elementBodyOffsetTop + childNode.offsetTop, (depth + 1) * STEP,
-						childNode.offsetHeight, STEP, 270, 270, getColour(depth));
+				facesHTML += getFaceHTML("top", "left",
+						childNode.offsetHeight, STEP, 270, 270, color);
                 childNode.insertAdjacentHTML('beforeend', facesHTML);
 			}
 		}
@@ -77,7 +73,7 @@ void(
 
 	traverse(body, 0, 0, 0);
 
-	var mode = "NO_FACES";
+	var mode = "FACES";
 	function move(e) {
 		if (mode !== "DISABLED") {
 			var xrel = e.screenX / screen.width;
@@ -89,43 +85,51 @@ void(
 	}
 
 	function mouseup(e) {
+        faces = document.getElementsByClassName("dom3d");
+
 		switch (mode) {
 		case "NO_FACES":
 			mode = "FACES";
-			faces.style.display = "";
+            for (let f of faces) {
+                f.style.display = "";
+            }
 			break;
 		case "FACES":
 			mode = "NO_FACES";
-			faces.style.display = "none";
+            for (let f of faces) {
+                f.style.display = "none";
+            }
 			break;
 		}
 	}
 
 	function stop(event) {
 		var key = event.code;
-		console.log(key)
 		if (key=="Escape") {
 			mode = "DISABLED";
-			r= false;
-			traverse(d, 0, 0, 0);
-			body.removeChild(f);
-			body.style = w;
+			r = false;
+			traverse(body, 0, 0, 0);
+            faces = document.getElementsByClassName("dom3d");
+            for (let f of faces) {
+			    body.removeChild(f);
+            }
 			document.removeEventListener("keydown", function(e) {stop(e)});
 			document.removeEventListener("keyup", function(e) {pause(e)});
 			document.removeEventListener("mousemove", function(b) {move(b)});
 			document.removeEventListener("mouseup", function() {mouseup()});
-		}
-		if (key=="ShiftLeft") {
-			mode = "DISABLED";
-			r= false;
 		}
 	}
 
 	function pause(event) {
 		var key = event.code;
 		if (key=="ShiftLeft") {
-			mode = "NO_FACES";
-			r = true;
+            if (mode == "NO_FACES") {
+                mode = "DISABLED";
+			    r = false;
+            } else {
+                mode = "NO_FACES";
+                r = true;
+            }
 		}
 	}
 
@@ -133,6 +137,6 @@ void(
 	document.addEventListener("mouseup", function() {mouseup()}, !0);
 
 	document.addEventListener("keydown", function(e) {stop(e)});
-	document.addEventListener("keyup", function(e) {pause(e)});
+	document.addEventListener("keydown", function(e) {pause(e)});
 
 } (25, 5000));
